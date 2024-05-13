28a2a1b8a35490cad7bb1d74f74048d9f396991f
Nicolass <nico.ojeda016@gmail.com>
2024-05-13 13:58:29 -0300

    changed/adapt-qr-for-pix auto-commit

---
+++ src/DefaultQr.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (C) 1997-2020 Reyesoft <info@reyesoft.com>.
+ *
+ * This file is part of CryptoQr. CryptoQr can not be copied and/or
+ * distributed without the express permission of Reyesoft
+ */
+
+declare(strict_types=1);
+
+namespace CryptoQr;
+
+class DefaultQr
+{
+    protected function getAddressProtocol(): string
+    {
+        return 'default';
+    }
+}
